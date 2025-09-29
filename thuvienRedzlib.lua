69d3e71ea16490a5857ac4cd69a0ee307402de29
naibelgodd <aunguyendangnguyen@gmail.com>
2025-09-30 00:37:39 +0700

    Update thuvienRedzlib.lua

--- thuvienRedzlib.lua
+++ thuvienRedzlib.lua
@@ -887,7 +887,7 @@ local redzlib = {
 }
 
 local ViewportSize = workspace.CurrentCamera.ViewportSize
-local UIScale = ViewportSize.Y / 450
+local UIScale = ViewportSize.Y / 350
 
 local Settings = redzlib.Settings
 local Flags = redzlib.Flags
